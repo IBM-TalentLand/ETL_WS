4691a35938df082628ebe4b3201203c43c99bd18
Gustavo Gonzalez Quintero <Gustavo.Gonzalez.Quintero1@ibm.com>
2018-04-01 23:39:31 -0500

    extraction changed again

--- src/main/scala/com/ibm/talentland/etl/extract/Extract.scala
+++ src/main/scala/com/ibm/talentland/etl/extract/Extract.scala
@@ -10,43 +10,43 @@ import scala.collection.mutable
 import scala.collection.mutable.ListBuffer
 
 object Extract {
-  var personBuffer = ListBuffer[SampleA]()
-  var debtsBuffer = ListBuffer[SampleB]()
+  var personBuffer = ListBuffer[Person]()
+  var debtsBuffer = ListBuffer[Debts]()
 
-  case class SampleA(
-    var ID: Long,
-    var Name: String,
-    var Phone: String,
-    var Country: String,
-    var Notes: String,
-    var Date: String
+  case class Person(
+    id: Long,
+    name: String,
+    phone: String,
+    country: String,
+    notes: String,
+    date: String
   )
 
-  case class SampleB(
-    var ID: Long,
-    var Income: String,
-    var Debt: Long,
-    var Payday_Limit: String
+  case class Debts(
+    id: Long,
+    income: String,
+    debt: Long,
+    payday_limit: String
   )
 
   def extract(): (DataFrame, DataFrame) = {
-    FileUtils.deleteFile("sample1.json", "sample2.json")
+    FileUtils.deleteFile("Person.json", "Debts.json")
     val cloudant = new Cloudant(
       AppEnv.getPropCloudant("cloudant.host"),
       AppEnv.getPropCloudant("cloudant.username"),
       AppEnv.getPropCloudant("cloudant.password")
     )
-    (getDFfromJson(cloudant, 1), getDFfromJson(cloudant, 2))
+    (getDFfromJson(cloudant, "Person"), getDFfromJson(cloudant, "Debts"))
   }
 
-  def getDFfromJson(cloudant: Cloudant, ntable: Int): DataFrame = {
+  def getDFfromJson(cloudant: Cloudant, nameTable: String): DataFrame = {
     FileUtils.writeJsonFile(
-      s"sample$ntable.json",
-      cloudant.getDocument("etl-workshop", s"Sample_$ntable").asString.body
+      s"$nameTable.json",
+      cloudant.getDocument("etl-workshop", nameTable).asString.body
     )
 
-    val sample = SparkSession.builder().master("local[*]").getOrCreate().read.json(s"sample$ntable.json")
-    if(ntable == 1){
+    val sample = SparkSession.builder().master("local[*]").getOrCreate().read.json(s"$nameTable.json")
+    if(nameTable == "Person"){
       sample.foreach(_.getAs[mutable.WrappedArray[GenericRowWithSchema]]("data").foreach(getDFRecords(_)))
       SparkSession.builder().getOrCreate().sqlContext.createDataFrame(personBuffer)
     } else {
@@ -56,10 +56,10 @@ object Extract {
   }
 
   def getDFRecords(row: Row): Unit = {
-    personBuffer += SampleA(row.getAs[Long]("ID"), row.getAs[String]("Name"), row.getAs[String]("Phone"), row.getAs[String]("Country"), row.getAs[String]("Notes"), row.getAs[String]("Date"))
+    personBuffer += Person(row.getAs[Long]("ID"), row.getAs[String]("Name"), row.getAs[String]("Phone"), row.getAs[String]("Country"), row.getAs[String]("Notes"), row.getAs[String]("Date"))
   }
 
   def getDFRecords2(row: Row): Unit = {
-    debtsBuffer += SampleB(row.getAs[Long]("ID"), row.getAs[String]("Income"), row.getAs[Long]("Debt"), row.getAs[String]("Payday_limit"))
+    debtsBuffer += Debts(row.getAs[Long]("ID"), row.getAs[String]("Income"), row.getAs[Long]("Debt"), row.getAs[String]("Payday_limit"))
   }
 }
